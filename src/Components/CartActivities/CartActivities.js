2a17d7c67f340c8fc9e3b58030ae6e3904a55d81
Tanzim Ahmed <utshotanzim@gmail.com>
2022-11-12 15:44:43 +0600

    persone info and break time added

--- src/Components/CartActivities/CartActivities.js
+++ src/Components/CartActivities/CartActivities.js
@@ -4,10 +4,26 @@ import "./CartActivities.css";
 
 const CartActivities = () => {
   return (
-    <div>
-      <img className="images" src={Img} alt="" />
-      <h3>Tanzim Ahmed Utsho</h3>
-      <h4>Dhaka,Bangladesh</h4>
+    <div className="cart">
+      <div>
+        <img className="images" src={Img} alt="" />
+        <h3>Tanzim Ahmed Utsho</h3>
+        <h4>Dhaka,Bangladesh</h4>
+      </div>
+      <div className="person-details">
+        <p>Weight:75kg</p>
+        <p>Height:6.1</p>
+        <p>Age:25yrs</p>
+      </div>
+
+      <p>Add A break</p>
+      <div className="time-break">
+        <p>5s</p>
+        <p>15s</p>
+        <p>25s</p>
+        <p>35s</p>
+        <p>45s</p>
+      </div>
     </div>
   );
 };
