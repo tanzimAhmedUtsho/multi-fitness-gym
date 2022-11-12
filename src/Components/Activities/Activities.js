6c72a116207f4e285a5631275da107f0ae1ba5e5
Tanzim Ahmed <utshotanzim@gmail.com>
2022-11-12 14:31:31 +0600

    conected data with website

--- src/Components/Activities/Activities.js
+++ src/Components/Activities/Activities.js
@@ -1,12 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Activities.css";
 import Img from "../../images/pp=01.png";
+import Exercise from "../Exercise/Exercise.js";
 
 const Activities = () => {
+  const [workouts, setWorkouts] = useState([]);
+
+  useEffect(() => {
+    fetch("fakeData.json")
+      .then((res) => res.json())
+      .then((data) => setWorkouts(data));
+  }, []);
   return (
     <div className="activities-container">
       <div>
         <h2>Select You Activities</h2>
+        {workouts.map((workout) => (
+          <Exercise key={workout.id}></Exercise>
+        ))}
       </div>
       <div>
         <img className="images" src={Img} alt="" />
---
+++ src/Components/Exercise/Exercise.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Exercise = () => {
+  return (
+    <div>
+      <h2>this is exersise</h2>
+    </div>
+  );
+};
+
+export default Exercise;
